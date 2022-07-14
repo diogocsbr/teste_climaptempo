3bf669951c260d79a9b575f53b4d583782831bb0
Diogo <diogo@codigocentral.com.br>
2022-07-14 19:14:43 -0300

    adicionando a página com vue.js

--- backend/teste.clima.api/teste.clima.models/ViewModel/CidadePrevisaoItemVM.cs
+++ backend/teste.clima.api/teste.clima.models/ViewModel/CidadePrevisaoItemVM.cs
@@ -9,6 +9,7 @@ namespace teste.clima.models.ViewModel
     public class CidadePrevisaoItemVM
     {
         public DateTime Data { get;set; }
+        public string DataStr { get { return this.Data.ToString("dd/MM/yyyy");  } }
         public string DiaSemana { get; set; } = string.Empty;
         public string Tempo { get; set; } = string.Empty;
         public decimal Minima { get; set; }
